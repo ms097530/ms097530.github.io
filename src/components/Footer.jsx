eca2b3423b9688ff9a1d67d5c3b351f0a5ff9467
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-30 16:03:22 -0400

    working on styling around bootstrap

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,20 @@ import ContactBar from "./ContactBar"
 import SocialBar from "./SocialBar"
 
 const useStyles = createUseStyles({
+    gradientContainer:
+    {
+        // backgroundColor: 'white',
+        backgroundImage: 'linear-gradient(to right, transparent 15%, var(--white) 15%, var(--white) 85%, transparent 85%)',
+        paddingTop: '1px'
+    },
     container:
     {
         fontFamily: 'var(--electrolize)',
         fontSize: '1rem',
         color: 'var(--white)',
-        borderTop: '1px solid var(--white)',
+        backgroundColor: 'var(--off-black)',
+        // height: '99%'
+        // borderTop: '1px solid var(--white)',
         paddingTop: '1.5rem'
     },
     link:
@@ -22,11 +30,13 @@ export default function Footer()
 {
     const classes = useStyles()
     return (
-        <div className={classes.container}>
-            <p>Built and designed by Michael Schultz</p>
-            <ContactBar />
-            <SocialBar />
-            <p>Icons by <a className={classes.link} href="https://icons8.com">Icons8</a></p>
+        <div className={classes.gradientContainer}>
+            <div className={classes.container}>
+                <p>Built and designed by Michael Schultz</p>
+                <ContactBar />
+                <SocialBar />
+                <p>Icons by <a className={classes.link} href="https://icons8.com">Icons8</a></p>
+            </div>
         </div>
     )
 }
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -5,7 +5,11 @@ export default function NavigationBar()
     return (
         <Navbar expand="lg" className="w-100">
             <Navbar.Brand href="#home">MS</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle aria-controls="basic-navbar-nav">
+                {/* <span> */}
+                <svg href="data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%280, 0, 0, 0.55%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e"></svg>
+                {/* </span> */}
+            </Navbar.Toggle>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
