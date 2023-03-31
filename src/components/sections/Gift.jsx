ae6e9a9edeac063aca1cf6eb77539e7c8a1eaf31
Michael Schultz <mikewschultz9029@gmail.com>
2023-03-31 09:38:36 -0400

    added dad joke functionality

--- src/components/sections/Gift.jsx
+++ src/components/sections/Gift.jsx
@@ -1,8 +1,48 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { createUseStyles } from 'react-jss'
 
+const useStyles = createUseStyles({
+    container:
+    {
+        color: 'var(--white)'
+    }
+})
 
 export default function Gift()
 {
+    const [joke, setJoke] = useState(null)
+    const classes = useStyles()
+
+    async function getJoke()
+    {
+        const res = await axios.get('https://icanhazdadjoke.com/', {
+            headers: {
+                Accept: 'application/json',
+                "User-Agent": 'Michael Schultz portfolio'
+            }
+        })
+        // console.log(res)
+        setJoke(res.data.joke)
+    }
+
+    useEffect(() =>
+    {
+        getJoke()
+    }, [])
+
+    const handleClick = async () =>
+    {
+        await getJoke()
+    }
+
     return (
-        <div>Gift</div>
+        <div className={classes.container}>
+            <h2>Gift</h2>
+            <p>{joke}</p>
+            <button onClick={handleClick}>
+                GET JOKE
+            </button>
+        </div>
     )
 }
