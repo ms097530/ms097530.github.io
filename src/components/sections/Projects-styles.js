c7295aa75d9186851f72ef6a53cb6cf4b92ca0a3
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-02 19:43:07 -0400

    removed unnecessary theme code

---
+++ src/components/sections/Projects-styles.js
@@ -0,0 +1,71 @@
+export default {
+    container:
+    {
+        color: 'var(--white)',
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        gap: '2rem',
+        marginBottom: '3rem'
+    },
+    electrolizeBody:
+    {
+        fontFamily: 'var(--electrolize)',
+        fontSize: '1.25rem'
+    },
+    card:
+    {
+        maxWidth: '500px',
+        border: '2px solid var(--dark-green)',
+        padding: '1rem 2rem'
+    },
+    title:
+    {
+        textTransform: 'uppercase',
+        textDecoration: 'underline',
+        fontFamily: 'var(--dosis-extra-light)',
+        color: 'var(--bright-green)',
+        fontSize: '2.75rem',
+        marginBottom: '2.5rem'
+    },
+    summary:
+    {
+        textAlign: 'left',
+        fontFamily: 'var(--electrolize)',
+        color: 'var(--white)',
+        fontSize: '1.25rem',
+        letterSpacing: '0.03rem',
+        lineHeight: '2.25rem'
+    },
+    techUsed:
+    {
+        color: 'var(--bright-green)',
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '1rem 2rem',
+        fontSize: '1rem',
+        marginBottom: '1.5rem'
+        // fontFamily: 'var(--electrolize)'
+    },
+    links:
+    {
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        listStyleType: 'none',
+        paddingInlineStart: '1rem',
+        gap: '2rem',
+        '& a':
+        {
+            color: 'var(--white)',
+            fill: 'var(--off-white)',
+            transition: 'color 300ms ease-in-out, fill 300ms ease-in-out',
+            textDecoration: 'none',
+            '&:hover':
+            {
+                color: 'var(--light-blue)',
+                fill: 'var(--light-blue)'
+            }
+        }
+    }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,42 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { createUseStyles, ThemeProvider } from 'react-jss'
-
-// const ThemeContext = React.createContext({})
-
-// const theming = createTheming(ThemeContext)
-// export const { ThemeProvider, useTheme } = theming
-
-// const useStyles = createUseStyles(
-//   {
-//     whiteText:
-//     {
-//       color: 'var(--white)'
-//     }
-//   },
-//   { theming }
-// )
-
-const myTheme = {
-  // '@global':
-  // {
-  // electrolize:
-  // {
-  //   fontFamily: 'var(--electrolize)',
-  //   color: 'var(--light-blue)'
-  // },
-  // }
-  '.span':
-  {
-    color: 'red'
-  }
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={myTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 )
