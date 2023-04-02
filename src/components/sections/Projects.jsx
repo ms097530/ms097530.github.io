4d286a1e0d7f6c9541023f382349eeccef5f16f4
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-02 19:42:03 -0400

    done with preliminary styling for projects

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,83 +1,14 @@
 import projectData from '../../data/project-data.js'
 import { createUseStyles, useTheme } from 'react-jss'
 import { ReactSVG } from 'react-svg'
+import styles from './Projects-styles.js'
 
-const useStyles = createUseStyles({
-    container:
-    {
-        color: 'var(--white)',
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        gap: '2rem',
-        marginBottom: '3rem'
-    },
-    electrolizeBody:
-    {
-        fontFamily: 'var(--electrolize)',
-        fontSize: '1.25rem'
-    },
-    card:
-    {
-        maxWidth: '500px',
-        border: '2px solid var(--dark-green)',
-        padding: '1rem 2rem'
-    },
-    title:
-    {
-        textTransform: 'uppercase',
-        textDecoration: 'underline',
-        fontFamily: 'var(--dosis-extra-light)',
-        color: 'var(--bright-green)',
-        fontSize: '2.5rem',
-        marginBottom: '2.5rem'
-    },
-    summary:
-    {
-        textAlign: 'left',
-        fontFamily: 'var(--electrolize)',
-        color: 'var(--white)',
-        fontSize: '1.25rem',
-        letterSpacing: '0.03rem',
-        lineHeight: '2.25rem'
-    },
-    techUsed:
-    {
-        color: 'var(--bright-green)',
-        display: 'flex',
-        flexWrap: 'wrap',
-        gap: '2rem',
-        fontSize: '1rem',
-        // fontFamily: 'var(--electrolize)'
-    },
-    links:
-    {
-        display: 'flex',
-        flexWrap: 'wrap',
-        alignItems: 'center',
-        listStyleType: 'none',
-        gap: '2rem',
-        '& a':
-        {
-            color: 'var(--white)',
-            fill: 'var(--off-white)',
-            transition: 'color 300ms ease-in-out, fill 300ms ease-in-out',
-            textDecoration: 'none',
-            '&:hover':
-            {
-                color: 'var(--light-blue)',
-                fill: 'var(--light-blue)'
-            }
-        }
-    }
-})
+const useStyles = createUseStyles(styles)
 
 export default function Projects()
 {
     const theme = useTheme()
-    console.log(theme)
     const classes = useStyles({ theme })
-    console.log(classes)
     return (
         <div className={classes.container}>
             {
