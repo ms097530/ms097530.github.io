9fc6972abfe689d22f0bcc571039ba5d35f7e0fc
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-03 18:37:12 -0400

    added headshot and styling to mission section

---
+++ src/components/sections/Mission-styles.js
@@ -0,0 +1,46 @@
+export default {
+    container:
+    {
+        '@media (min-width: 700px)':
+        {
+            display: 'flex',
+            gap: '2rem',
+            alignItems: 'center'
+        }
+    },
+    textContainer:
+    {
+        '@media (min-width: 700px)':
+        {
+            maxWidth: '50%',
+            marginInline: 'auto'
+        }
+    },
+    text:
+    {
+        color: 'var(--off-white)',
+        fontFamily: 'var(--electrolize)',
+        fontSize: '1.25rem',
+        letterSpacing: '0.03em',
+        lineHeight: '2.25rem'
+    },
+    imgContainer:
+    {
+        maxWidth: '200px',
+        marginInline: 'auto',
+        overflow: 'hidden',
+        border: '3px solid var(--more-off-white)',
+        '& img':
+        {
+            width: '100%',
+            filter: 'drop-shadow(0px 4px 20px rgba(238, 238, 238, 0.35))'
+        },
+        '@media (min-width: 700px)':
+        {
+            maxWidth: '300px',
+            borderTop: 'none',
+            borderRight: 'none',
+            overflow: 'visible'
+        }
+    }
+}
--- src/components/sections/Mission.jsx
+++ src/components/sections/Mission.jsx
@@ -1,29 +1,23 @@
 import { createUseStyles } from 'react-jss'
+import styles from './Mission-styles'
 
-const useStyles = createUseStyles({
-    text:
-    {
-        color: 'var(--off-white)',
-        fontFamily: 'var(--electrolize)',
-        fontSize: '1.25rem',
-        letterSpacing: '0.03em',
-        lineHeight: '1.5rem'
-    }
-})
+const useStyles = createUseStyles(styles)
 
 export default function Mission()
 {
     const classes = useStyles()
     return (
-        <div>
-            <p className={classes.text}>
-                I guarantee that you&apos;ll be blown away by the work I provide. I&apos;m a team player who isn&apos;t afraid of leading the charge or going solo.
-            </p>
-            <p className={classes.text}>
-                As such, I give results that speak for themselves. Frontend or backend, I offer the solutions that you need for your projects.
-            </p>
+        <div className={classes.container}>
+            <div className={classes.textContainer}>
+                <p className={classes.text}>
+                    I guarantee that you&apos;ll be blown away by the work I provide. I&apos;m a team player who isn&apos;t afraid of leading the charge or going solo.
+                </p>
+                <p className={classes.text}>
+                    As such, I give results that speak for themselves. Frontend or backend, I offer the solutions that you need for your projects.
+                </p>
+            </div>
             <div className={classes.imgContainer}>
-                <img className={classes.img} src="#" alt="my headshot" />
+                <img className={classes.img} src="/img/headshot-small-nobg.png" alt="my headshot" />
             </div>
         </div>
     )
