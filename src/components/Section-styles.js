70b481dbdab24ed69c60d8e627935fd64a87f79f
Michael Schultz <mikewschultz9029@gmail.com>
2023-04-03 20:30:12 -0400

    refactored section styles to separate file for consistency

---
+++ src/components/Section-styles.js
@@ -0,0 +1,13 @@
+export default {
+    title:
+    {
+        fontFamily: 'var(--dosis-extra-light)',
+        fontSize: '3rem',
+        color: 'var(--white)',
+        textDecoration: 'underline',
+        textTransform: 'uppercase',
+        letterSpacing: '0.08em',
+        marginTop: '4rem',
+        marginBottom: '4rem',
+    }
+}
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,18 +1,7 @@
 import { createUseStyles } from "react-jss"
+import styles from './Section-styles'
 
-const useStyles = createUseStyles({
-    title:
-    {
-        fontFamily: 'var(--dosis-extra-light)',
-        fontSize: '3rem',
-        color: 'var(--white)',
-        textDecoration: 'underline',
-        textTransform: 'uppercase',
-        letterSpacing: '0.08em',
-        marginTop: '4rem',
-        marginBottom: '4rem',
-    }
-})
+const useStyles = createUseStyles()
 
 export default function Section({ title, children })
 {
